5456cc36d1c296a6539255e75d1f1e6ffa89118d
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-12 14:42:30 +0500

    implement premiers and announcements section

--- pages/components/FindMovies.js
+++ pages/components/FindMovies.js
@@ -8,8 +8,10 @@ import fonts from '../../styles/Fonts.module.css'
 export default function FindMovies() {
 
   const [active, setActive] = useState("movies")
+  const [premierActive, setPremierActive] = useState("premierMovies")
   const [movies, setMovies] = useState([])
   const [featuredToday, setFeaturedToday] = useState([])
+  const [premiersAndAnnouncements, setPremiersAndAnnouncements] = useState([])
 
   const handleChange = async (e) => {
     const movieName = e.target.value;
@@ -45,24 +47,40 @@ export default function FindMovies() {
   };
 
   const fetchFeaturedContent = (url) => {
-    // fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}`)
     fetch(`${url}?api_key=${process.env.TMDB_API_KEY}`)
     .then((response) => response.json())
     .then((data) => setFeaturedToday(data))
-  }
+  } // end of fetchFeaturedContent
+
+  const fetchPremiersAndAnnouncements = (url) => {
+    fetch(`${url}?api_key=${process.env.TMDB_API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => setPremiersAndAnnouncements(data))
+  } // end of fetchPremiersAndAnnouncements
 
   useEffect(() => {
     fetchFeaturedContent("https://api.themoviedb.org/3/trending/movie/day")
+    fetchPremiersAndAnnouncements("https://api.themoviedb.org/3/movie/upcoming")
   }, [])
 
   const showMovies = () => {
     setActive("movies")
     fetchFeaturedContent("https://api.themoviedb.org/3/trending/movie/day")
-  } 
+  } // end of showMovies
 
   const showSeries = () => {
     setActive("series")
     fetchFeaturedContent("https://api.themoviedb.org/3/tv/airing_today")
+  } // end of showSeries
+
+  const showPremierMovies = () => {
+    setPremierActive("premierMovies")
+    fetchPremiersAndAnnouncements("https://api.themoviedb.org/3/movie/upcoming")
+  }
+
+  const showPremierSeries = () => {
+    setPremierActive("premierSeries")
+    fetchPremiersAndAnnouncements("https://api.themoviedb.org/3/tv/on_the_air")
   }
   
   const loadDataDebounced = useDebounce(handleChange, 1000)
@@ -98,16 +116,16 @@ export default function FindMovies() {
           </div> 
         }
 
-        <p className={`text-[#EFD839] self-start mx-12 mt-20 text-3xl`}> Featured Today </p>
-        <nav className='mt-5 self-start mx-12 text-xl w-[96%]'>
-          <ul className='flex gap-5'> 
-            <li className={`hover:cursor-pointer ${active === "movies" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showMovies}> Movies </li>
-            <li className={`hover:cursor-pointer ${active === "series" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showSeries}> Series </li>
-          </ul>
-          <hr className='mt-[-2px] border-red-500 border-b-2'/>
-        </nav>
-
       {/* Featured today Movies/TV Series */}
+      <p className={`text-[#EFD839] self-start mx-12 mt-20 text-3xl`}> Featured Today </p>
+      <nav className='mt-5 self-start mx-12 text-xl w-[96%]'>
+        <ul className='flex gap-5'> 
+          <li className={`hover:cursor-pointer ${active === "movies" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showMovies}> Movies </li>
+          <li className={`hover:cursor-pointer ${active === "series" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showSeries}> Series </li>
+        </ul>
+        <hr className='mt-[-2px] border-red-500 border-b-2'/>
+      </nav>
+
       <div className='mb-10 max-w-[100%] overflow-auto ml-1 mr-1 self-start flex h-[450px] gap-5'>
         {featuredToday.results && featuredToday.results.map((movie, index) => (
           <div className='mt-2 flex flex-col gap-2 m-3'>
@@ -123,6 +141,29 @@ export default function FindMovies() {
       </div>
 
       {/* Premiers and Announcements */}
+      <p className={`text-[#EFD839] self-start mx-12 mt-20 text-3xl`}> Premiers and Announcements </p>
+      <nav className='mt-5 self-start mx-12 text-xl w-[96%]'>
+        <ul className='flex gap-5'> 
+          <li className={`hover:cursor-pointer ${premierActive === "premierMovies" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showPremierMovies}> Movies </li>
+          <li className={`hover:cursor-pointer ${premierActive === "premierSeries" ? 'text-white border-b-4 border-white' : 'text-[#7d7d7d]'} pb-[2px] px-5`} onClick={showPremierSeries}> Series </li>
+        </ul>
+        <hr className='mt-[-2px] border-red-500 border-b-2'/>
+      </nav>
+
+      <div className='mb-10 max-w-[100%] overflow-auto ml-1 mr-1 self-start flex h-[450px] gap-5'>
+        {premiersAndAnnouncements.results && premiersAndAnnouncements.results.map((movie, index) => (
+          <div className='mt-2 flex flex-col gap-2 m-3'>
+          <Image style={{ minWidth: '200px', objectFit: 'cover' }} className='rounded-xl self-center mx-1'
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            width={200}
+            height={200}
+            alt='Poster image' 
+          />
+            <p className='w-[80%] self-center text-center mt-2 text-[#c0c0c0]'> {premierActive === "premierMovies" ? movie.title : movie.name} </p>
+          </div>
+        ))}
+      </div>
+
 
       </div>
     </div>
