74990913bb5138c0eb60a4cfda432165e6706476
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-15 23:35:55 +0500

    create default export with React component

--- pages/URLs.js
+++ pages/URLs.js
@@ -1,3 +1,4 @@
+import React from 'react'
 export const TMDB_MOVIE_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie'
 export const TMDB_URL = 'https://api.themoviedb.org/3/movie'
 export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/original'
@@ -6,3 +7,12 @@ export const FEATURED_TODAY_MOVIES_URL = 'https://api.themoviedb.org/3/trending/
 export const PREMIER_MOVIES_URL = 'https://api.themoviedb.org/3/movie/upcoming'
 export const FEATURED_TODAY_SERIES_URL = 'https://api.themoviedb.org/3/tv/airing_today'
 export const PREMIER_SERIES_URL = "https://api.themoviedb.org/3/tv/on_the_air"
+
+export default function URLs() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
