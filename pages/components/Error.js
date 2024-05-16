ff9ebfe079948cdc2d4f40cab2bb4da71cf16047
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-16 20:27:59 +0500

    attach click event listener to refresh button in Error.js

--- pages/components/Error.js
+++ pages/components/Error.js
@@ -7,6 +7,10 @@ import Head from 'next/head'
 
 export default function Error() {
 
+  const handleRefresh = () => {
+    window.location.reload()
+  }
+
   return (
     <>
       <Head>
@@ -23,7 +27,7 @@ export default function Error() {
           <Image className='mx-auto mt-20' src={plugError} alt='plug' width={250} height={250} />
           <p className={`text-white text-3xl ${fonts.latoRegular}`}> Oops... </p>
           <p className={`text-[#585858] text-xl ${fonts.latoBold}`}> Something went wrong </p>
-          <button className={`bg-yellow-500 w-32 h-12 rounded-md hover:cursor-pointer border-2 hover:border-white hover:bg-black hover:text-white transition-all duration-500 text-[20px] text-center ${fonts.latoMeduim} py-2 px-3`}> Refresh </button>
+          <button className={`bg-yellow-500 w-32 h-12 rounded-md hover:cursor-pointer border-2 hover:border-white hover:bg-black hover:text-white transition-all duration-500 text-[20px] text-center ${fonts.latoMeduim} py-2 px-3`} onClick={handleRefresh}> Refresh </button>
         </div>
       </div>
     </>
